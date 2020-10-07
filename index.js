2b4103c217d80be88c40dd9c56c68b6ba2706734
takaakikoyama <me@takaakikoyama.com>
2020-10-07 16:51:32 +0900

    add command options.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-var connect     = require('connect'),
+const package_json = require("./package.json");
+const { program } = require('commander');
+let connect     = require('connect'),
     serveStatic = require('serve-static'),
     ssi         = require('connect-ssi'),
     logger      = require('connect-logger'),
-    baseDir     = __dirname + '/htdocs';
-var app = connect();
+    baseDir     = __dirname + '/htdocs',
+    app = connect();
+
+program
+  .version(package_json.version)
+  .option('-p, --port <number>', 'port number', (arg)=>{ return parseInt(arg); }, 3000)
+  .option('-d, --dir <path>', 'docment root dir', baseDir)
+  .parse(process.argv);
+
+console.log('start http server')
+console.log(`docment root: ${program.dir}`)
+console.log(`http://localhost:${program.port}/`)
 app.use(logger());
 app.use(ssi({
-  baseDir: baseDir,
+  baseDir: program.dir,
   ext: '.html'
 }));
-app.use(serveStatic(baseDir));
-app.listen(3000);
+app.use(serveStatic(program.dir));
+app.listen(program.port);
